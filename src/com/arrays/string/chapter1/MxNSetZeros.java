043ad0e794e7558fcd4d8fe19fd01545bc9309db
Pramoda <pramod.majhi@gmail.com>
2021-08-19 09:08:30 -0700

    modified MxNSet

--- src/com/arrays/string/chapter1/MxNSetZeros.java
+++ src/com/arrays/string/chapter1/MxNSetZeros.java
@@ -26,7 +26,7 @@ public class MxNSetZeros {
 		int matrix[][] =  new int[][]
 			{
 				{ 1, 1 }, 
-				{ 1, 1 } 
+				{ 0, 1 } 
 			};
 		
 		display(matrix);
@@ -34,6 +34,23 @@ public class MxNSetZeros {
 		System.out.println("**********");
 		display(matrix);
 
+	}
+	
+	// Nullifying row
+	public static void nullifyRow(int matrix[][], int row) {
+		for (int i = 0; i < matrix[0].length; i++) {
+			matrix[row][i] = 0 ;
+		}
+	}
+	
+	// Nullify col
+	
+	public static void nullifyCol(int matrix[][], int col) {
+		
+		for (int i = 0; i < matrix.length; i++) {
+			matrix[i][col] = 0;
+			
+		}
 	}
 
 	public static void setZeros(int matrix[][]) {
@@ -64,7 +81,41 @@ public class MxNSetZeros {
 			}			
 		}
 		
+		// check for zeros in the rest of the array
 		
+		for(int i=1; i < matrix.length; i ++) {
+			for(int j=1;  j < matrix[0].length; j++) {
+				if(matrix[i][j] == 0) {
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
+		
+		
+		// Nullify  rows based on values in first column
+		for (int i = 0; i < matrix.length; i++) {
+			if(matrix[i][0] == 0) {
+				nullifyCol(matrix, i);
+			}
+		}
+		
+		// Nullify  rows based on values in first row
+				for (int i = 0; i < matrix[0].length; i++) {
+					if(matrix[0][i] == 0) {
+						nullifyCol(matrix, i);
+					}
+				}
+				
+		// nullify first row
+				if(rowHasZero) {
+					nullifyRow(matrix, 0);
+				}
+				
+		// Nullify first column
+		if(colHasZero) {
+			nullifyRow(matrix, 0);
+		}
 		
 	}
 	
