96268bf1aa49231257d3add5054d58cb9ff54cf2
Pramoda <pramod.majhi@gmail.com>
2021-09-11 07:01:18 -0700

    binary tree related

--- src/com/javascript-practice/binarytree/treemin.js
+++ src/com/javascript-practice/binarytree/treemin.js
@@ -10,11 +10,11 @@ Space: O(n)
  */
 
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 const a = new Node(5);
@@ -39,19 +39,68 @@ c.right = f;
 // in this case ->3
 const treeMinValue = (a) => {
 
-// let solve this issue using recursions, also depth first search
- if(a == null) return Infinity;
-const smallestLeft = treeMinValue(a.left);
-const smallestRight = treeMinValue(a.right);
+    // let solve this issue using recursions, also depth first search
+    if (a == null) return Infinity;
+    const smallestLeft = treeMinValue(a.left);
+    const smallestRight = treeMinValue(a.right);
 
-return Math.min(a.val, smallestLeft, smallestRight);
+    return Math.min(a.val, smallestLeft, smallestRight);
 
 };
-const minValue  = treeMinValue(a);
+const minValue = treeMinValue(a);
 console.log(minValue);
 
 // depth first search iterative 
+// DFS = Stack
+const treeMinValueDfs = (root) => {
+    let stack = [root];
+    let smallest = Infinity;
+    while(stack.length > 0) {
+        let current = stack.pop();
+        if(current.val < smallest) {
+            smallest = current.val;
+        } 
+        if(current.left !== null) stack.push(current.left);
+        if(current.right !=null) stack.push(current.right);
 
-const treeMinValueDfs = (a) => {
-    
+    }
+    return smallest;
 }
+
+const dfsMin = treeMinValueDfs(a);
+console.log(dfsMin);
+
+// BFS solution .. Queue  , we can't solve using recursive
+// Only solution we have is iterative
+
+/**
+ * n = number of nodes
+Time: O(n)
+Space: O(n)
+Note: this solution should really be considered O(n^2) runtime because 
+the JavaScript shift() methods runs in O(n). 
+JavaScript does not have a native queue data structure that is maximally efficient.
+ * @param {*} root 
+ * @returns 
+ */
+const bsfTreeMin = (root) => {
+    
+    const queue = [root];
+    let smallest = Infinity;
+    while(queue.length > 0) {
+        /**
+         * The shift() method removes the first element 
+         * from an array and returns that removed element. 
+         * This method changes the length of the array.
+         */
+        let current = queue.shift();// get the element from queue.
+        if(current.val < smallest) {
+            smallest = current.val;
+        }
+        if(current.left !== null) queue.push(current.left);
+        if(current.right !== null) queue.push(current.right);
+    }
+    return smallest;
+};
+const bsf = bsfTreeMin(a);
+console.log(bsf);
