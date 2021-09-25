46a3120943548136fa3a1d928e095ad904d89a73
Pramoda <pramod.majhi@gmail.com>
2021-09-24 22:45:13 -0700

    moved xls to root

--- src/com/javascript-practice/sortingAndSearching/anagram.js
+++ src/com/javascript-practice/sortingAndSearching/anagram.js
@@ -7,22 +7,22 @@
 
 const anagramGroup = (arr) => {
 
-   let mapStrArray = arr.map(s => {
-       let sortVal = s.split('').sort().join('');
-       return {
-           orignal: s,
-           sorted: sortVal
-       }
-   })
-   //console.log(mapStrArray);
-   mapStrArray.sort((a,b) => {
-       return a.sorted < b.sorted ? 1 : -1;
-   })
+    let mapStrArray = arr.map(s => {
+        let sortVal = s.split('').sort().join('');
+        return {
+            orignal: s,
+            sorted: sortVal
+        }
+    })
+    //console.log(mapStrArray);
+    mapStrArray.sort((a, b) => {
+        return a.sorted < b.sorted ? 1 : -1;
+    })
 
-   let answer = mapStrArray.map(m => {
-       return m.orignal;
-   })
-   return answer;
+    let answer = mapStrArray.map(m => {
+        return m.orignal;
+    })
+    return answer;
 }
 
 let s = ['rat', 'car', 'tar'];
@@ -45,15 +45,40 @@ const anagrams = [
     // 'ninethumps',
     // 'theclassroom'
     "apple", "banana", "carrot", "ele", "duck", "papel", "tarroc", "cudk", "eel", "lee"
-  ];
-  const anagramsSorted = ["ninethumps", "punishments", "dormitory", "dirtyroom", "astronomer", "moonstarrer", "motherinlaw", "hitlerwoman", "thequeershakes", "schoolmaster", "theclassroom", "badcredit", "theearthquakes", "debit card"];
+];
+const anagramsSorted = ["ninethumps", "punishments", "dormitory", "dirtyroom", "astronomer", "moonstarrer", "motherinlaw", "hitlerwoman", "thequeershakes", "schoolmaster", "theclassroom", "badcredit", "theearthquakes", "debit card"];
 
 // console.log(anagramGroup(anagrams));
 // groupAnagrams can be sorted
-let answer  = JSON.stringify(anagramGroup(anagrams)) 
+let answer = JSON.stringify(anagramGroup(anagrams))
 // let input = JSON.stringify(anagramsSorted);
 // if(answer === input) {
 //     console.log(true);
 // }
 console.log(answer);
 // console.log(input);
+/**
+ * Given an array of strings strs, group the anagrams together. 
+ * You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a 
+different word or phrase, 
+typically using all the original letters exactly once.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ * @param {*} strs 
+ */
+const groupAnagram = (strs) => {
+    let cache = {};
+    for (let str of strs) {
+        let sortedKey = str.split('').join(''); // aet
+        (!cache[sortedKey]) ? cache[sortedKey] = [str] : cache[sortedKey].push(str);
+    }
+    return Object.values(cache);
+}
+let strs = ["eat","tea","tan","ate","nat","bat"]
+const a = groupAnagram(strs);
+console.log(a);
